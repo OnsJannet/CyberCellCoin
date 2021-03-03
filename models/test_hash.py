e6842aa3fd52f05227337b446d53d361d08361aa
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-03-03 20:34:48 +0000

    seccess

--- models/test_hash.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import hashlib
-m = hashlib.sha256()
-m.update(b"Nobody inspects")
-m.update(b" the spammish repetition")
-print(m.digest())
-print(m.digest_size)
-print(m.block_size)
