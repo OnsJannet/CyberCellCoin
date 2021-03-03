3c94e9bf1dfeb8d5927693ba49a34ff000b1ddb2
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2021-03-03 20:47:00 +0000

    cleaning code

--- models/chain.py
+++ models/chain.py
@@ -6,26 +6,17 @@ class Chain:
     def __init__(self):
         self.code = Hash()
         self.chain = []
-        self.new_hash = None
-
+        save = {'prev':None,
+                'index':0,
+                'block':"First of his name"}
+        self.chain.append(save)
+    
     def add_block(self, block):
-        if(self.chain == []):
-            save = {'prev':None,
-                    'index':0,
-                    'block':"First of his name"}
-            self.chain.append(save)
-
-            save = {'prev': self.code.hash(self.chain[-1]['block']),
-                    'index':self.chain[-1]['index'] + 1,
-                    'block':block.__dict__}
-            self.chain.append(save)
-        else:
-            save = {'prev': self.code.hash(self.chain[-1]['block']),
-                    'index':self.chain[-1]['index'] + 1,
-                    'block':block.__dict__}
-            self.chain.append(save)
+        save = {'prev': self.code.hash(self.chain[-1]['block']),
+                'index':self.chain[-1]['index'] + 1,
+                'block':block.__dict__}
+        self.chain.append(save)
 
     def dict_chain(self):
         return self.chain
-
 
