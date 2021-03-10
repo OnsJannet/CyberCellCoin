aac93479685a1bb7b1aca6e19715cd0d52e3f363
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-03-10 21:33:40 +0100

    update work

--- models/hash.py
+++ models/hash.py
@@ -12,3 +12,28 @@ class Hash:
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return sha256(encoded_block).hexdigest()
+
+
+    def proof_of_work(self, block):
+        """
+        Function that tries different values of nonce to get a hash
+        that satisfies our difficulty criteria.
+        """
+        block.nonce = 0
+
+        computed_hash = block.hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.compute_hash()
+
+        return computed_hash
+
+
+    def is_valid_proof(self, block, block_hash):
+        """
+        Check if block_hash is valid hash of block and satisfies
+        the difficulty criteria.
+        """
+        return (block_hash.startswith('0' * Blockchain.difficulty) and
+                block_hash == block.hash())
+                
