f0665f039aae9b7717641064e345b3b7ec01c240
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-03-12 21:53:22 +0100

    test cases

--- models/2-test.py
+++ models/2-test.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 from wallet import Wallet
 
-my_wallet = Wallet(user_id)
+my_wallet = Wallet("bahlous")
 
 
-my_wallet.generate_RSA()
+my_wallet.create_keys()
 my_wallet.save_keys()
+my_wallet.load_keys()
+pub = my_wallet.public_key
+prv = my_wallet.private_key
+print(pub)
+a = my_wallet.user_id
+print(a)
