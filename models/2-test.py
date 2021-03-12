a2631229303444537413f26af30b1943f838f3ee
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-03-12 21:59:59 +0100

    only load keys

--- models/2-test.py
+++ models/2-test.py
@@ -4,11 +4,9 @@ from wallet import Wallet
 my_wallet = Wallet("bahlous")
 
 
-my_wallet.create_keys()
-my_wallet.save_keys()
 my_wallet.load_keys()
 pub = my_wallet.public_key
 prv = my_wallet.private_key
-print(pub)
+print(pub, prv)
 a = my_wallet.user_id
-print(a)
+print("current user is:", a)
