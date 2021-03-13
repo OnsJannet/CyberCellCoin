dc945e49a83f0038ed0ca3bb80a8ba507b4d9799
Aymen-haddaji-hub <aymensystem7@gmail.com>
2021-03-13 20:11:41 +0100

    i think i need to seperate the transaction from the blockchain

---
+++ models/transactions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module to handle all the transactions process """
+from wallet import Wallet
+from datetime import datetime
+
+class Transaction:
+    def __init__(self, sender, recipient, amount):
+        """
+        Creates a new transaction
+        :param sender: <str> sender account
+        :param recipient: <str> recipient account
+        :param amount: <float> amount to be transferred
+        """
+        self.sender = sender
+        self.recipient = recipient
+        self.amount = amount
+        self.signature = signature
+
+    def validate(self):
+        """
+        Checks if a transaction is valid
+        :return: <bool> True if it is valid, False if not.
+        """
+
+        # Prevent stealing by creating negative transactions
+        if self.amount < 0:
+            return False
+
+        return True
+
+    def to_dict(self):
+        return ordreddict([('sender', self.sender), ('recipient', self.recipient), ('amount', self.amount)])
