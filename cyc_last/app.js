b590186d74d346bd6e7e154c47691a42fd24a0ea
onsjannet <onsjannet@gmail.com>
2021-03-18 23:00:48 +0100

    redid everything

---
+++ cyc_last/app.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+// Display Mobile Menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu);
