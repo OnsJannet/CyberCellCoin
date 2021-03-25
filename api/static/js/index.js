a27684d8963aebc64a1790ca1e40d44ab5583e0e
onsjannet <onsjannet@gmail.com>
2021-03-25 20:21:16 +0100

    final version

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,3 +1,13 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
 /*-1-----------------------------------*/
         $(document).on('click','.member-1',function(){
             $('.detail-box-1').toggleClass('show-details-1')
