3ff4ab05144ec9a7e0e9aae86a12821391f26e0a
onsjannet <onsjannet@gmail.com>
2021-03-26 10:59:17 +0100

    final version

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,3 +1,4 @@
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
@@ -8,6 +9,38 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight')
+    const homeMenu = document.querySelector('#home-page')
+    const aboutMenu = document.querySelector('#about-page')
+    const teamMenu = document.querySelector('#team-page')
+    let scrollPos = window.scrollY
+
+
+if(window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight')
+    aboutMenu.classList.remove('highlight')
+    return;
+} else if (window.innerWidth > 960 && scrollPos < 1400){
+    aboutMenu.classList.add('highlight')
+    homeMenu.classList.remove('highlight')
+    teamMenu.classList.remove('highlight')
+    return;
+} else if (window.innerWidth > 960 && scrollPos < 2345){
+    teamMenu.classList.add('highlight')
+    aboutMenu.classList.remove('highlight')
+    return;
+}
+
+if ((elem && window.innerWidth <960 && scrollPos <600) || elem) {
+        elem.classList.remove('highlight');
+    }
+};
+
+window.addEventListener('scroll', highlightMenu );
+window.addEventListener('click', highlightMenu );
+
 /*-1-----------------------------------*/
         $(document).on('click','.member-1',function(){
             $('.detail-box-1').toggleClass('show-details-1')
